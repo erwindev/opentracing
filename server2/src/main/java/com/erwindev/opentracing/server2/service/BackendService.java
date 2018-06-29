70374485f1d8fb978128de61acbf3a647bd22247
Erwin Alberto <erwin.alberto@availity.com>
2018-06-29 11:42:44 -0400

    added Hystrix to server2

--- server2/src/main/java/com/erwindev/opentracing/server2/service/BackendService.java
+++ server2/src/main/java/com/erwindev/opentracing/server2/service/BackendService.java
@@ -1,5 +1,9 @@
 package com.erwindev.opentracing.server2.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,8 +15,20 @@ public class BackendService {
     @Autowired
     private RestTemplate restTemplate;
 
+    private static final Logger logger = LoggerFactory.getLogger(BackendService.class);
+
+    @HystrixCommand(fallbackMethod = "handleError", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4"),
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000")})
     public String process(){
+        logger.info("Calling server3");
         ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9083/hello", String.class);
+        logger.info("Returned from server3");
         return response.getBody();
+    }
+
+    private String handleError() {
+        logger.info("server3 is down!");
+        return "Degraded Hello Service (server3 is down)!";
     }
 }
